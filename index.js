b22a04768692d0742f12d55d4c13fa36365420dd
attempt2 <mickey@gmail.com>
2025-01-31 16:25:05 +0530

    made numpad functional

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+//functional Buttons
 let resultBtn=document.getElementById("result");
 let question=document.getElementById("question");
 let inArea=document.getElementById("inputArea");
 let generateBtn=document.getElementById("generate");
 let submitBtn=document.getElementById("submit");
 let sum=0;
-submitBtn.addEventListener('click',()=>{
-    let valueEntered=inArea.value;
-    if (valueEntered=="hello"){
-        resultBtn.innerText="yayy";
-    }
-})
+let inputValue=0;
+
+//buttons of numpad;
+let oneBtn=document.getElementById("one");
+let twoBtn=document.getElementById("two");
+let threeBtn=document.getElementById("three");
+let fourBtn=document.getElementById("four");
+let fiveBtn=document.getElementById("five");
+let sixBtn=document.getElementById("six");
+let sevenBtn=document.getElementById("seven");
+let eightBtn=document.getElementById("eight");
+let nineBtn=document.getElementById("nine");
+let zeroBtn=document.getElementById("zero");
+let backSpaceBtn=document.getElementById("backspace");
+
+//
 
 generateBtn.addEventListener('click',()=>{
     inArea.value="";
@@ -41,4 +52,52 @@ submitBtn.addEventListener('click',()=>{
         resultBtn.innerText=`Oops, Correct Answer: ${sum}`;
     }
 })
+
+// making numpad functional 
+
+oneBtn.addEventListener('click',()=>{
+
+})
+backSpaceBtn.addEventListener('click',()=>{
+    inArea.value=Math.trunc(inArea.value/10);
+    if(inArea.value==0){
+        inArea.value="";
+    }
+})
+oneBtn.addEventListener('click',()=>{
+    updateInputValue(1);
+})
+twoBtn.addEventListener('click',()=>{
+    updateInputValue(2);
+})
+threeBtn.addEventListener('click',()=>{
+    updateInputValue(3);
+})
+fourBtn.addEventListener('click',()=>{
+    updateInputValue(4);
+})
+fiveBtn.addEventListener('click',()=>{
+    updateInputValue(5);
+})
+sixBtn.addEventListener('click',()=>{
+    updateInputValue(6);
+})
+sevenBtn.addEventListener('click',()=>{
+    updateInputValue(7);
+})
+eightBtn.addEventListener('click',()=>{
+    updateInputValue(8);
+})
+nineBtn.addEventListener('click',()=>{
+    updateInputValue(9);
+})
+zeroBtn.addEventListener('click',()=>{
+    updateInputValue(0);
+})
+function updateInputValue(a){
+    
+    inArea.value=(inArea.value)*10+a;
+}
+
+
 
